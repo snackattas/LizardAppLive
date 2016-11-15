f91a7ba103a37e1d8bcaf6de3459f0a451d9d1b2
snackattas <zach.attas@gmail.com>
2016-11-15 02:27:46 +0000

    switched to postgres, modified requirements.txt

--- pkg/main.py
+++ pkg/main.py
@@ -16,6 +16,7 @@ from urllib2 import urlopen
 from functools import wraps
 import datetime
 import os
+import arrow
 
 store = HttpExposedFileSystemStore(
     path=os.path.join(os.path.dirname(__file__), "images"))
@@ -137,42 +138,42 @@ def pretty_date(time=False):
         time [default=False]: A python date object
     Returns: The time difference between the time passed in and now, in
              human-readable form"""
-    now = datetime.datetime.now()
-    if type(time) is int:
-        diff = now - datetime.fromtimestamp(time)
-    elif isinstance(time,datetime.datetime):
-        diff = now - time
-    elif not time:
-        diff = now - now
-    second_diff = diff.seconds
-    day_diff = diff.days
-
-    if day_diff < 0:
-        return ""
-
-    if day_diff == 0:
-        if second_diff < 10:
-            return "just now"
-        if second_diff < 60:
-            return str(second_diff) + " seconds ago"
-        if second_diff < 120:
-            return "a minute ago"
-        if second_diff < 3600:
-            return str(second_diff / 60) + " minutes ago"
-        if second_diff < 7200:
-            return "an hour ago"
-        if second_diff < 86400:
-            return str(second_diff / 3600) + " hours ago"
-    if day_diff == 1:
-        return "yesterday"
-    if day_diff < 7:
-        return str(day_diff) + " days ago"
-    if day_diff < 31:
-        return str(day_diff / 7) + " weeks ago"
-    if day_diff < 365:
-        return str(day_diff / 30) + " months ago"
-    return str(day_diff / 365) + " years ago"
-
+    # now = datetime.datetime.now()
+    # if type(time) is int:
+    #     diff = now - datetime.fromtimestamp(time)
+    # elif isinstance(time,datetime.datetime):
+    #     diff = now - time
+    # elif not time:
+    #     diff = now - now
+    # second_diff = diff.seconds
+    # day_diff = diff.days
+    #
+    # if day_diff < 0:
+    #     return ""
+    #
+    # if day_diff == 0:
+    #     if second_diff < 10:
+    #         return "just now"
+    #     if second_diff < 60:
+    #         return str(second_diff) + " seconds ago"
+    #     if second_diff < 120:
+    #         return "a minute ago"
+    #     if second_diff < 3600:
+    #         return str(second_diff / 60) + " minutes ago"
+    #     if second_diff < 7200:
+    #         return "an hour ago"
+    #     if second_diff < 86400:
+    #         return str(second_diff / 3600) + " hours ago"
+    # if day_diff == 1:
+    #     return "yesterday"
+    # if day_diff < 7:
+    #     return str(day_diff) + " days ago"
+    # if day_diff < 31:
+    #     return str(day_diff / 7) + " weeks ago"
+    # if day_diff < 365:
+    #     return str(day_diff / 30) + " months ago"
+    # return str(day_diff / 365) + " years ago"
+    return arrow.get(time).humanize()
 
 def recentActivity():
     """recentActivity: Returns the 10 latest changes (add, delete, update) to
