ec222bca5bd6769b931d2510313742f71c517a14
Zattas <zach.attas@gmail.com>
2016-11-16 19:31:25 +0000

    turn off debug mode

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.WARNING)
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
-app.debug = True
+# app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] =\
 #     'postgresql://lizardappuser:password@localhost/lizardapp'
